7ea5aee57702c22967545d84d5f812b684e09f05
joshhouston <jjhouston24@gmail.com>
2019-12-28 18:15:58 -0600

    landing

---
+++ src/components/Landing.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class Landing extends Component{
+    constructor(){
+        super();
+        this.state = {
+        }
+    }
+    render(){
+        return (
+            <div className="landing">
+                <div className="search">
+                    <div className="search-text">
+                        <h2>CHECK PLAYER AND RANK STATS</h2>
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Landing;
